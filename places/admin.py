db9d1686b6309491ff455edbdc1ce5182fa0d1a5
Weddsigell <gorbunov.kirill.grigorievich@gmail.com>
2025-11-24 17:47:51 +0500

    добавил предпросмотр картинок в джанго

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from .models import Image, Place
+from django.utils.html import format_html
 
 
 class PlaceImageInline(admin.TabularInline):
     model = Image
     extra = 1
+    fields = ('image', 'get_preview', 'position')
+    readonly_fields = ('get_preview',)
+
+
+    @admin.display(description="Предпросотр")
+    def get_preview(self, obj):
+        return format_html(
+            '<img src="{}" style="max-height:200px; width:auto;" />',
+            obj.image.url,
+        )
 
 
 @admin.register(Place)
@@ -20,10 +31,21 @@ class PlaceAdmin(admin.ModelAdmin):
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = (
-        'id',
         'place_title',
+        'get_preview',
+        'position'
     )
+    readonly_fields = ('get_preview',)
 
 
+    @admin.display(description="Предпросотр")
+    def get_preview(self, obj):
+        return format_html(
+            '<img src="{}" style="max-height:200px; width:auto;" />',
+            obj.image.url,
+        )
+
+
+    @admin.display(description="Место")
     def place_title(self, obj):
         return obj.place.title
